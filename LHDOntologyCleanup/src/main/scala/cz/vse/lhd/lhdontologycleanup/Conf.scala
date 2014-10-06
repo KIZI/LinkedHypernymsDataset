3ae43bab4c7d8fae81c120d3c0a943ac886eaa7a
propi <prozeman@gmail.com>
2014-10-06 20:05:25 +0200

    OntologyCleanup update

--- LHDOntologyCleanup/src/main/scala/cz/vse/lhd/lhdontologycleanup/Conf.scala
+++ LHDOntologyCleanup/src/main/scala/cz/vse/lhd/lhdontologycleanup/Conf.scala
@@ -2,31 +2,39 @@ package cz.vse.lhd.lhdontologycleanup
 
 import cz.vse.lhd.core.AppConf
 import cz.vse.lhd.core.ConfGlobal
+import cz.vse.lhd.core.FileExtractor
 import java.io.FileInputStream
+import java.io.IOException
 
 object Conf extends ConfGlobal {
-  
+
   val (
     globalPropertiesFile,
-    datasetInstance_typesPath,
-    datasetInstance_typesEnPath,
-    datasetOntologyPath,
-    datasetInterlanguage_linksEnPath,
     manualmappingOverridetypesPath,
     manualmappingExcludetypesPath
-  ) = {
+    ) = {
     val prop = buildProp(new FileInputStream(AppConf.args(0)))
     (
       prop.getProperty("global.properties.file"),
-      prop.getProperty("dataset.instance_types.path"),
-      prop.getProperty("dataset.instance_types.en.path"),
-      prop.getProperty("dataset.ontology.path"),
-      prop.getProperty("dataset.interlanguage_links.en.path"),
       prop.getProperty("manualmapping.overridetypes.path"),
-      prop.getProperty("manualmapping.excludetypes.path")
-    )
+      prop.getProperty("manualmapping.excludetypes.path"))
   }
-  
+
+  val (
+    datasetInstance_typesPath,
+    datasetInstance_typesEnPath,
+    datasetOntologyPath,
+    datasetInterlanguage_linksEnPath) = (
+    s"${Conf.datasetsDir}instance_types_$lang.nt",
+    s"${Conf.datasetsDir}instance_types_en.nt",
+    s"${Conf.datasetsDir}dbpedia_${Conf.dbpediaVersion}.owl",
+    s"${Conf.datasetsDir}interlanguage_links_en.nt")
+
+  List(datasetInstance_typesPath, datasetInstance_typesEnPath, datasetOntologyPath, datasetInterlanguage_linksEnPath) foreach {
+    case FileExtractor(_) =>
+    case x => throw new IOException(s"File $x does not exist or is not writable.")
+  }
+
 }
 
 object Logger extends cz.vse.lhd.core.Logger {
