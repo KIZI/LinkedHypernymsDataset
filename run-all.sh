3d5b13a16ebf5e3a46f58b9bc1a893cda60b771e
Vaclav Zeman <qzemv01@boa.lmcloud.vse.cz>
2014-10-11 15:54:12 +0200

    run-all.sh file added

--- run-all.sh
+++ run-all.sh
@@ -1,4 +1,30 @@
 #!/bin/sh
 
-# Set these paths appropriately
+# New LHD version downloading + installing
+git pull
+mvn clean
+mvn install
 
+# Downloading of required DBpedia datasets
+(cd Downloader; mvn scala:run -DaddArgs=../global.properties)
+
+# DBpedia resources indexing (short abstracts + types)
+(cd HypernymExtractor; mvn scala:run -Dlauncher=indexer -DaddArgs=module.properties)
+
+# Removing of old output LHD files
+rm -R data/output/*
+
+# Hypernym Extraction (short abstract analysis -> tree-tagger -> Hypernym extraction by JAPE Grammar -> map hypernyms to DBpedia resources)
+# One Thread
+#(cd HypernymExtractor; mvn scala:run -Dlauncher=runner -DaddArgs=module.properties)
+# Multi Thread
+(cd MapReduce; mvn scala:run -Dlauncher=starter -DaddArgs=10000)
+
+# Mapping of DBpedia resources to DBpedia ontology by text-matching; Resources cleaning
+(cd LHDOntologyCleanup; mvn scala:run -DaddArgs=module.properties)
+
+# Mapping of all DBpedia resource to DBpedia ontology by statistical type inference
+# Creating of draft files LHDv1.0, LHDv2.0, plainHyp
+(cd LHDTypeInferrer; mvn scala:run -DaddArgs=module.properties)
+
+echo "++ LHD EXTRACTION PROCESS WAS SUCCESSFUL ++"
