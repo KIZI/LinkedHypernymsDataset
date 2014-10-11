b260f0bb920df41d84edbac9b6cc5444af48402c
propi <prozeman@gmail.com>
2014-10-11 17:20:40 +0200

    Lang correct

--- MapReduce/src/main/scala/cz/vse/lhd/mapreduce/cmd/HypenymExtractionCommand.scala
+++ MapReduce/src/main/scala/cz/vse/lhd/mapreduce/cmd/HypenymExtractionCommand.scala
@@ -8,9 +8,9 @@ class HypenymExtractionCommand(
 ) extends Command {
 
   def execute = {
-    Logger.get.info(s"New extraction process have been started: ${startPointer} - ${endPointer}")
+    Logger.get.info(s"New extraction process has been started: ${startPointer} - ${endPointer}")
     MavenCommandReceiver(s"""scala:run -Dlauncher=runner -DaddArgs="module.properties|$startPointer|$endPointer" """.trim, HypenymExtractionCommand.homeDir)
-    Logger.get.info(s"The extraction process have been finished: ${startPointer} - ${endPointer}")
+    Logger.get.info(s"The extraction process has been finished: ${startPointer} - ${endPointer}")
   }
   
 }
