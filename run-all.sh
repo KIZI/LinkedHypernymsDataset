813e9a9b773c5ea8a9d9dfc4a9d42ff1dab50c7b
Václav Zeman <prozeman@gmail.com>
2015-06-09 13:51:25 +0200

    SH Script update

--- run-all.sh
+++ run-all.sh
@@ -27,23 +27,7 @@ mvn install
 # Downloading of required DBpedia datasets
 (cd Downloader; mvn scala:run -DaddArgs=$1) || failure "Failed to download all datasets"
 
-# DBpedia resources indexing (short abstracts + types)
-(cd HypernymExtractor; mvn scala:run -Dlauncher=indexer -DaddArgs=$1) || failure "Failed to index datasets"
-
-# Removing of old output LHD files
-rm -R data/output/*
-
-# Hypernym Extraction (short abstract analysis -> tree-tagger -> Hypernym extraction by JAPE Grammar -> map hypernyms to DBpedia resources)
-# One Thread
-#(cd HypernymExtractor; mvn scala:run -Dlauncher=runner -DaddArgs=$1)
-# Multi Thread
-(cd MapReduce; mvn scala:run -Dlauncher=starter -DaddArgs="$1|10000") || failure "Failed to extract all hypernyms"
-
-# Mapping of DBpedia resources to DBpedia ontology by text-matching; Resources cleaning
-(cd LHDOntologyCleanup; mvn scala:run -DaddArgs=$1) || failure "Failed to clean hypernym datasets"
-
-# Mapping of all DBpedia resource to DBpedia ontology by statistical type inference
-# Creating of draft files LHDv1.0, LHDv2.0, plainHyp
-(cd LHDTypeInferrer; mvn scala:run -DaddArgs=$1) || failure "Failed to infer types of hypernyms"
+# LHD Pipeline
+(cd Pipeline; mvn scala:run -DaddArgs="$1|remove-all") || failure "LHD Generation Failed"
 
 echo "++ LHD EXTRACTION PROCESS WAS SUCCESSFUL ++"
